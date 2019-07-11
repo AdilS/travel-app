0452b1fcbe82949e7e74bbe77afca35f37606149
AdilS <adilcareer@gmail.com>
2019-07-11 23:18:42 +0530

    router change

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import DashboardLayout from '../src/layouts/DashboardLayout';
+
 const App = () => {
   return (
     <div className="App">
--- client/src/layouts/DashboardLayout.js
+++ client/src/layouts/DashboardLayout.js
@@ -1,15 +1,22 @@
 import React from "react";
 import Left from "../components/Left";
 import Header from "../components/Header";
-import Content from "../components/Content";
+import AddCategory from "../components/AddCategory";
+import ViewCategory from "../components/ViewCategory";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 class DashboardLayout extends React.Component {
   render() {
     return (
       <frameElement>
         <Left />
         <div id="right-panel" className="right-panel">
-        <Header />
-        <Content />
+          <Header />
+          <Router>
+            <div>
+              <Route path='/addcategory' component={AddCategory} />
+              <Route path='/viewcategory' component={ViewCategory} />
+            </div>
+          </Router>
         </div>
       </frameElement>
     );
