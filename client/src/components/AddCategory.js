8bc395b9d2ba616a0105e44ad6b620f7f6fda4eb
AdilS <adilcareer@gmail.com>
2019-07-11 23:25:52 +0530

    new files

---
+++ client/src/components/AddCategory.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import '../assets/css/cs-skin-elastic.css';
+import '../assets/css/style.css';
+class AddCategory extends Component{
+
+    render(){
+        return (
+            <div>Add Category</div>
+           
+        )
+    }
+}
+export default AddCategory;
---
+++ client/src/components/ViewCategory.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import '../assets/css/cs-skin-elastic.css';
+import '../assets/css/style.css';
+class ViewCategory extends Component{
+
+    render(){
+        return (
+            <div>Hello cate</div>
+           
+        )
+    }
+}
+export default ViewCategory;
